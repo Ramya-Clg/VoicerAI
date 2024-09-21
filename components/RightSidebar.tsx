5ed722f05e92c2967cc6b673541b2bdecd8fb7f8
Ramya <202201409@daiict.ac.in>
2024-09-22 00:02:01 +0530

    add left sidebar

---
+++ components/RightSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <section className='right_sidebar'>
+      Rightsidebar
+    </section>
+  )
+}
+
+export default RightSidebar
