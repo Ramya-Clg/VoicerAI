13290e439d8edd43e9c74e5e9a60005099a9e308
Ramya <202201409@daiict.ac.in>
2024-09-22 14:02:13 +0530

    add boilerplate convex

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,26 @@
+"use client";
 import PodcastCard from "@/components/PodcastCard";
 import { Button } from "@/components/ui/button";
 import { podcastData } from "@/constants";
 import exp from "constants";
 import React from "react";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 const Home = () => {
+    const tasks = useQuery(api.tasks.get);
     return <div className="mt-9 flex flex-col gap-9">
         <section className="flex flex-col gap-5">
             <h1 className="text-20 font-bold text-white-1">
                 Trending podcasts
             </h1>
+            <div className="text-white-1 flex min-h-screen flex-col items-center justify-between p-24">
+                {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+            </div>
             <div className="podcast_grid">
-            {podcastData.map((podcast) => {
-                return <PodcastCard key={podcast.id} imgURL={podcast.imgURL} description={podcast.description} title={podcast.title} podcastID = {podcast.id} />
-            })}
+                {podcastData.map((podcast) => {
+                    return <PodcastCard key={podcast.id} imgURL={podcast.imgURL} description={podcast.description} title={podcast.title} podcastID={podcast.id} />
+                })}
             </div>
         </section>
     </div>;
